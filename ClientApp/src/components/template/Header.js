f0d302249a40d9f92c0b1559c4f556b7abffc725
irsat000 <irsat000@gmail.com>
2023-03-13 23:30:05 +0300

    main page development

--- ClientApp/src/components/template/Header.js
+++ ClientApp/src/components/template/Header.js
@@ -1,17 +1,19 @@
 import { Search, List, PersonCircle, CaretDownFill } from 'react-bootstrap-icons';
 
 
-const DesktopHeader = () => {
+const DesktopHeader = (props) => {
     return (
         <div className="desktop_header">
             <div className="page_title">
-                <span>Sektörler</span>
+                <span>Anasayfa</span>
             </div>
             <div className='header_actions'>
-                <div className="search-desktop-cont">
-                    <input type="text" placeholder="Marka ara" />
-                    <Search className='input_search-icon' />
-                </div>
+                {props.page !== "Anasayfa" ?
+                    <div className="search-header_desktop-cont">
+                        <input type="text" placeholder="Marka ara" />
+                        <Search className='search_input-icon' />
+                    </div> : null
+                }
                 <div className="menu-cont">
                     <div className="account">
                         <PersonCircle />
@@ -26,36 +28,25 @@ const DesktopHeader = () => {
 }
 
 
-
-
-export const IndexHeader = () => {
-    return (
-        <header className="app_header">
-            <div className="mobile_header-index">
-                <div className="drawerBtn-cont">
-                    <List />
-                </div>
-            </div>
-            <DesktopHeader />
-        </header>
-    )
-};
-
-export const AppHeader = () => {
+export const AppHeader = (props) => {
     return (
         <header className="app_header">
             <div className="mobile_header">
                 <div className="drawerBtn-cont">
                     <List />
                 </div>
-                <div className="logo-mobile-cont">
-                    <span>MarkaSkor</span>
-                </div>
-                <div className="search-mobile-cont">
-                    <Search />
-                </div>
+                {props.page !== "Anasayfa" ?
+                    <>
+                        <div className="logo-mobile-cont">
+                            <span>MarkaSkor</span>
+                        </div>
+                        <div className="search-mobile-cont">
+                            <Search />
+                        </div>
+                    </> : null
+                }
             </div>
-            <DesktopHeader />
+            <DesktopHeader page={props.page} />
         </header>
     )
 };
--- ClientApp/src/pages/Home.js
+++ ClientApp/src/pages/Home.js
@@ -1,17 +1,94 @@
-import { } from 'react-bootstrap-icons';
-import { IndexHeader, AppHeader } from '../components/template/Header';
+import { Search } from 'react-bootstrap-icons';
+import { AppHeader } from '../components/template/Header';
 import { NavDesktop } from '../components/template/NavDesktop';
 
 export const HomePage = () => {
     return (
         <div className='page_content'>
             <NavDesktop />
-            <aside>
-                <AppHeader />
-                <main>
-                    Hi!
+            <section>
+                <AppHeader page="Anasayfa" />
+                <main className='main-sectors'>
+                    <div className='mainpage_heading'>
+                        <span>MarkaSkor</span>
+                    </div>
+                    <div className="search-main-cont">
+                        <input type="text" placeholder="Marka ara" />
+                        <Search className='search_input-icon' />
+                    </div>
+                    <div className='sectors_heading'>
+                        <span>Sektörler</span>
+                    </div>
+                    <article>
+                        <div className='sector_item'>
+                            <div className='sector_item_img-cont'>
+                                <img src={require('../assets/images/sectors/goods.png')} alt='Tüketim' />
+                            </div>
+                            <span>Tüketim</span>
+                        </div>
+                        <div className='sector_item'>
+                            <div className='sector_item_img-cont'>
+                                <img src={require('../assets/images/sectors/technology.png')} alt='Teknoloji' />
+                            </div>
+                            <span>Teknoloji</span>
+                        </div>
+                        <div className='sector_item'>
+                            <div className='sector_item_img-cont'>
+                                <img src={require('../assets/images/sectors/fashion.png')} alt='Moda' />
+                            </div>
+                            <span>Moda</span>
+                        </div>
+                        <div className='sector_item'>
+                            <div className='sector_item_img-cont'>
+                                <img src={require('../assets/images/sectors/retail.png')} alt='Perakende' />
+                            </div>
+                            <span>Perakende</span>
+                        </div>
+                        <div className='sector_item'>
+                            <div className='sector_item_img-cont'>
+                                <img src={require('../assets/images/sectors/automotive.png')} alt='Otomotiv' />
+                            </div>
+                            <span>Otomotiv</span>
+                        </div>
+                        <div className='sector_item'>
+                            <div className='sector_item_img-cont'>
+                                <img src={require('../assets/images/sectors/education.png')} alt='Eğitim' />
+                            </div>
+                            <span>Eğitim</span>
+                        </div>
+                        <div className='sector_item'>
+                            <div className='sector_item_img-cont'>
+                                <img src={require('../assets/images/sectors/finance.png')} alt='Finans' />
+                            </div>
+                            <span>Finans</span>
+                        </div>
+                        <div className='sector_item'>
+                            <div className='sector_item_img-cont'>
+                                <img src={require('../assets/images/sectors/energy.png')} alt='Enerji' />
+                            </div>
+                            <span>Enerji</span>
+                        </div>
+                        <div className='sector_item'>
+                            <div className='sector_item_img-cont'>
+                                <img src={require('../assets/images/sectors/travel.png')} alt='Seyahat' />
+                            </div>
+                            <span>Seyahat</span>
+                        </div>
+                        <div className='sector_item'>
+                            <div className='sector_item_img-cont'>
+                                <img src={require('../assets/images/sectors/health.png')} alt='Sağlık' />
+                            </div>
+                            <span>Sağlık</span>
+                        </div>
+                        <div className='sector_item'>
+                            <div className='sector_item_img-cont'>
+                                <img src={require('../assets/images/sectors/service.png')} alt='Servis' />
+                            </div>
+                            <span>Servis</span>
+                        </div>
+                    </article>
                 </main>
-            </aside>
+            </section>
         </div>
     )
 };
