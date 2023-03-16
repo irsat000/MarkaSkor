4b0bd194b61fa66137a3fa58f981fb834d25b441
irsat000 <irsat000@gmail.com>
2023-03-16 21:45:50 +0300

    Desktop nav and mobile drawer design

--- ClientApp/src/components/template/Header.tsx
+++ ClientApp/src/components/template/Header.tsx
@@ -1,16 +1,56 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { Search, List, PersonCircle, CaretDownFill, Gear } from 'react-bootstrap-icons';
+import { Search, List, PersonCircle, CaretDownFill, Gear, Shuffle, Newspaper, ArrowLeftShort } from 'react-bootstrap-icons';
 
 
 
 const Drawer = (props: {
-    isActive: boolean,
-    toggleDrawer: () => void
+    drawerActive: boolean,
+    toggleDrawer: () => void,
+    closeDrawer: () => void,
 }) => {
-    return (
-        <div className={`drawer-cont${props.isActive ? ' active' : ''}`} id='mobile_drawer' onClick={props.toggleDrawer}>
-            <nav className='drawer'>
+    const refDrawer = useRef<any>(null);
 
+    const handleClickOutsideDrawer = (event: any) => {
+        if (refDrawer.current && !refDrawer.current.contains(event.target)) {
+            props.closeDrawer();
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutsideDrawer);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutsideDrawer);
+        };
+    }, []);
+
+    return (
+        <div className={`drawer-cont${props.drawerActive ? ' active' : ''}`} id='mobile_drawer'>
+            <nav className='drawer' ref={refDrawer}>
+                <div className='dr-account-cont'>
+                    <div className='dr-closeDrawer' onClick={props.toggleDrawer}>
+                        <ArrowLeftShort />
+                    </div>
+                    <div className='dr-profilePic-cont'>
+                        <img src={require('../../assets/images/SiteIcon.png')} alt="MarkaSkor Icon" />
+                    </div>
+                    <div className='dr-account_info'>
+                        <span className='dr-account_username'>Muhammedİrşat</span>
+                        <span className='dr-account_email'>irsat000@gmail.com</span>
+                    </div>
+                </div>
+                <div className='dr-link_groups-cont'>
+                    <span className='dr-group_heading'>Account</span>
+                    <ul className='dr-group_account'>
+                        <li><a href='/'><PersonCircle className='dr-link_icon' /><span>Giriş yap</span></a></li>
+                        <li><a href='/'><PersonCircle className='dr-link_icon' /><span>Kayıt ol</span></a></li>
+                    </ul>
+                    <span className='dr-group_heading'>Genel</span>
+                    <ul className='dr-group_general'>
+                        <li><a href='/'><Shuffle className='dr-link_icon' /><span>Marka kıyasla</span></a></li>
+                        <li><a href='/'><Newspaper className='dr-link_icon' /><span>Haberler</span></a></li>
+                        <li><a href='/'><Gear className='dr-link_icon' /><span>Ayarlar</span></a></li>
+                    </ul>
+                </div>
             </nav>
         </div>
     )
@@ -90,6 +130,9 @@ export const AppHeader = (props: {
     const toggleDrawer = () => {
         setDrawerActive(!drawerActive);
     };
+    const closeDrawer = () => {
+        setDrawerActive(false);
+    }
 
     return (
         <header className='app_header'>
@@ -109,7 +152,7 @@ export const AppHeader = (props: {
                 }
             </div>
             <DesktopHeader page={props.page} />
-            <Drawer isActive={drawerActive} toggleDrawer={toggleDrawer} />
+            <Drawer drawerActive={drawerActive} toggleDrawer={toggleDrawer} closeDrawer={closeDrawer} />
         </header>
     )
 };
--- ClientApp/src/components/template/NavDesktop.tsx
+++ ClientApp/src/components/template/NavDesktop.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import { } from 'react-bootstrap-icons';
-
-
+import { Shuffle, Newspaper } from 'react-bootstrap-icons';
 
 export const NavDesktop = () => {
     return (
         <nav className='desktop_nav'>
-            <div className='nav_logo-cont'>
-                <a href='/'>MarkaSkor</a>
-            </div>
+            <a href='/' className='nav_logo-cont'>
+                <img src={require('../../assets/images/SiteIcon.png')} alt="MarkaSkor Icon" />
+                <span>MarkaSkor</span>
+            </a>
+            <ul className='nav_useful_links'>
+                <li>
+                    <div className='nav_link_icon-cont'><Shuffle /></div>
+                    <span>Marka kıyasla</span>
+                </li>
+                <li>
+                    <div className='nav_link_icon-cont'><Newspaper /></div>
+                    <span>Haberler</span>
+                </li>
+            </ul>
         </nav>
     )
 };
